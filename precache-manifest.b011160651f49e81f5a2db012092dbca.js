e350c36803b2356ace52131702bde35dbec3f1d0
sneha-jaz <snehajais03@gmail.com>
2020-06-01 14:13:51 +0530

    Updates

---
+++ precache-manifest.b011160651f49e81f5a2db012092dbca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae1fecacaa3a9f2459ca4ecb1f3d70f",
+    "url": "/To-Do-App/index.html"
+  },
+  {
+    "revision": "6fc8f29b6e3ee1d13cc7",
+    "url": "/To-Do-App/static/css/main.60dd91c4.chunk.css"
+  },
+  {
+    "revision": "b4640184e044cb130d37",
+    "url": "/To-Do-App/static/js/2.ebfedda5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-App/static/js/2.ebfedda5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc8f29b6e3ee1d13cc7",
+    "url": "/To-Do-App/static/js/main.33b94fce.chunk.js"
+  },
+  {
+    "revision": "95d99890b72831bf3eec",
+    "url": "/To-Do-App/static/js/runtime-main.eb50b0ec.js"
+  },
+  {
+    "revision": "6375717a86bdcd558c522aff052e27d2",
+    "url": "/To-Do-App/static/media/logo.6375717a.svg"
+  }
+]);
